93218635fab10665b4ba4687d02f3b7bca1cf0cf
YuanJie <wangjh0825@qq.com>
2025-12-19 17:00:39 +0800

    feat(logger): 增强日志记录功能支持文件输出和轮转
    - 添加文件日志输出支持
    - 实现日志文件按日期分割
    - 配置普通日志和错误日志分离存储
    - 设置日志文件大小轮转规则
    - 添加日志保留时间和压缩策略
    - 统一控制台和文件日志格式

--- core/logger.py
+++ core/logger.py
@@ -1,14 +1,50 @@
 import sys
 from loguru import logger
 from contextvars import ContextVar
+from pathlib import Path
 
 request_id_ctx = ContextVar("request_id", default="-")
 
+BASE_DIR = Path(__file__).resolve().parent
+LOG_ROOT = BASE_DIR / "logs"
+
 logger.remove()
+
+# 文件
+
+LOG_FORMAT = (
+    "{time:YYYY-MM-DD HH:mm:ss.SSS} | "
+    "{level:<8} | "
+    "{extra[request_id]} | "
+    "{message}"
+)
+# 控制台
 logger.add(
     sys.stdout,
-    format="{time} {level} {message} [{extra[request_id]}]",
+    format=LOG_FORMAT,
     level="INFO",
+)
+
+# 所有日志
+logger.add(
+    LOG_ROOT / "{time:YYYY-MM-DD}/app.log",
+    format=LOG_FORMAT,
+    level="INFO",
+    rotation="100 MB",  # 单文件最大 100MB
+    retention="5 days",
+    compression="zip",
+    enqueue=True,
+)
+
+
+# 错误日志单独文件
+logger.add(
+    LOG_ROOT / "{time:YYYY-MM-DD}/error.log",
+    format=LOG_FORMAT,
+    level="ERROR",
+    rotation="100 MB",  # 单文件最大 100MB
+    retention="7 days",
+    enqueue=True,
 )
 
 
